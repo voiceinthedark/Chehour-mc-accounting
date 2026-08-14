ece348aff4863635735a3cf9d91bbe857855e3fc
voiceinthedark <darkrisingforce@gmail.com>
2026-08-14 11:01:18 +0300

    web: pagination functionality
    feat(doctorSettings): add pagination functionality

--- apps/web-display/src/pages/settings/DoctorSettings.jsx
+++ apps/web-display/src/pages/settings/DoctorSettings.jsx
@@ -1,20 +1,30 @@
 // filepath: apps/web-display/src/pages/settings/DoctorSettings.jsx
 
-import { Typography, Stack, Box } from "@mui/material";
+import { Typography, Box, Pagination } from "@mui/material";
 import "@fontsource/almarai";
 import { useLoaderData } from "react-router-dom";
 import { useEffect, useState } from "react";
-import DoctorRow from "../../components/Rows/DoctorRow";
 import DoctorCard from "../../components/Cards/DoctorCard";
 import "./doctorSettings.scss";
 
 const DoctorSettings = () => {
+  // TODO: implement adding a new doctor functionality
+
   const [services, setServices] = useState([]);
   // Use the useLoaderData hook to access the data loaded by the loader function
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6; // Number of items to display per page
   const data = useLoaderData();
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const paginatedData = data.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage,
+  );
+
   useEffect(() => {
     console.log("Loaded data:", data);
-    // TODO: Use the loaded data to populate the settings form or display relevant information
+    console.log("Paginated data:", paginatedData);
   }, [data]);
 
   // Fetch services from the backend API
@@ -29,7 +39,7 @@ const DoctorSettings = () => {
   }, []);
 
   return (
-    <div style={{ padding: "20px", width: "100%" }}>
+    <div className="main-container" style={{ width: "100%" }}>
       <Typography
         variant="h3"
         component="h1"
@@ -38,10 +48,18 @@ const DoctorSettings = () => {
         إعدادات الأطباء
       </Typography>
       <Box className="doctor-settings-container" component="form">
-        {(data || []).map((doctor) => (
-          <DoctorCard key={doctor.id} doctor={doctor} />
+        {paginatedData.map((doctor) => (
+          <DoctorCard key={doctor.id} doctor={doctor} services={services} />
         ))}
       </Box>
+
+      <Pagination
+        count={Math.ceil(data.length / itemsPerPage)}
+        page={currentPage}
+        onChange={(event, value) => setCurrentPage(value)}
+        color="primary"
+        style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+      />
     </div>
   );
 };
